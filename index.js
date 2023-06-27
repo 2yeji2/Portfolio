49786dfaa8981918329f05ac71a0299d6e91ee27
2yeji2 <ii316@naver.com>
2023-06-27 17:59:32 +0900

    파일추가

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ $(".open_btn").on("click", () => {
 
 // 메뉴안 contact 버튼 닫기
 $(".contactbox")
-  .children("svg:last-child")
+  .children(".close_btn")
   .on("click", () => {
     $(".contactbox").css({ display: "none" });
   });
+
+// a태그 막기
+$(function () {
+  $(".open_btn").on("click", function (e) {
+    e.preventDefault();
+  });
+});
