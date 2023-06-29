178723c02ecf758bd247be1a816128edf7978b17
2yeji2 <ii316@naver.com>
2023-06-29 18:05:47 +0900

    0629파일추가

--- index.js
+++ index.js
@@ -15,9 +15,32 @@ $(".open_btn").on("click", (evt) => {
   evt.preventDefault();
 });
 
-//다크모드 토글
-$(".mode").on("click", () => {
-  $(".dark").addClass("light");
-  $(".dark").children(".txt").text("Light Mode");
-  $(".dark_svg").appendTo(".dark");
+//다크모드 적용
+$(".toggle_box span").on("click", () => {
+  $("#first-main").toggleClass("dark_mode");
+  $(".title").toggleClass("dark_mode");
+  $(".suv-title").toggleClass("dark_mode");
+  $(".menu").toggleClass("dark_mode");
+  $("#second-main").toggleClass("dark_mode");
+  $(".me1").toggleClass("dark_mode");
+  $(".pers").children("p").eq(0).toggleClass("dark_mode");
+  $(".pers").children("p").eq(1).toggleClass("dark_mode");
+  $(".pers").children("p").eq(2).toggleClass("dark_mode");
+  $(".pers").children("p").eq(3).toggleClass("dark_mode");
+  $(".pers").children("p").eq(4).toggleClass("dark_mode");
+  $(".pers").children("p").eq(5).toggleClass("dark_mode");
+  $("#third-main").toggleClass("dark_mode");
+  $(".skill").children("p").eq(0).toggleClass("dark_mode");
+  $(".skill").children("p").eq(1).toggleClass("dark_mode");
+  $(".skill").children("p").eq(2).toggleClass("dark_mode");
+  $(".skill").children("p").eq(3).toggleClass("dark_mode");
+  $(".skill").children("p").eq(4).toggleClass("dark_mode");
+  $(".skill").children("p").eq(5).toggleClass("dark_mode");
+  $(".skill").children("p").eq(6).toggleClass("dark_mode");
+  $(".skill").children("p").eq(7).toggleClass("dark_mode");
+  $("#fourth-main").toggleClass("dark_mode");
+  $(".project_name").toggleClass("dark_mode");
+  $(".gotoPj").toggleClass("dark_mode");
+  $(".project-img").toggleClass("dark_mode");
+  $(".box").toggleClass("dark_mode");
 });
