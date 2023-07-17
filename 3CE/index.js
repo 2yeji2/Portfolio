8351cf720acd4e60a365618acf37458c0b65ad9a
2yeji2 <ii316@naver.com>
2023-07-17 18:03:17 +0900

    상세페이지추가

--- 3CE/index.js
+++ 3CE/index.js
@@ -1,12 +1,28 @@
+$(window)
+  .resize(function () {
+    if (window.innerWidth > 1200) {
+      // 슬라이드
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        scrollbar: {
+          el: ".swiper-scrollbar",
+          hide: false,
+        },
+      });
+    } else {
+      var swiper = new Swiper(".mySwiper", {
+        scrollbar: {
+          el: ".swiper-scrollbar",
+          hide: false,
+        },
+      });
+    }
+  })
+  .resize();
+
 // 햄버거메뉴
 $(".checkbox").on("click", () => {
-  $(".menu").slideToggle(); // == display: block
-});
-
-// 슬라이드
-var swiper = new Swiper(".mySwiper", {
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    hide: false,
-  },
+  $(".mo-tb-menu").slideToggle();
+  $(".mo-tb-menu").toggleClass("hide");
 });
---
+++ Project/Project1/index.js
@@ -0,0 +1,34 @@
+//fade up
+AOS.init();
+
+//다크모드
+$(".toggle_box span").on("click", () => {
+  $("header").children("a").toggleClass("dark_mode");
+  $("#project1").toggleClass("dark_mode");
+  $("#project1").children(".title").toggleClass("dark_mode");
+  $(".threece").children("h1").toggleClass("dark_mode");
+  $(".threece").children("ul").children("li").toggleClass("dark_mode");
+  $("#overview").toggleClass("dark_mode");
+  $("#overview").children("h3").toggleClass("dark_mode");
+  $(".overview").children("ul").children("li").toggleClass("dark_mode");
+  $("#design").toggleClass("dark_mode");
+  $("#design").children("h3").toggleClass("dark_mode");
+  $(".design")
+    .children("ul")
+    .children("li")
+    .children("h4")
+    .toggleClass("dark_mode");
+  $(".design")
+    .children("ul")
+    .children("li")
+    .children("p")
+    .toggleClass("dark_mode");
+  $(".design").children(".pallet").toggleClass("dark_mode");
+  $("#publishing").toggleClass("dark_mode");
+  $("#publishing").children("h3").toggleClass("dark_mode");
+  $(".publishing")
+    .children("ul")
+    .children("li")
+    .children("h4")
+    .toggleClass("dark_mode");
+});
