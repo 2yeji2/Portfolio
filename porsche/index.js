2c64c7fd163f5a28abebe347d5cbb3d4bfde2f0c
2yeji2 <ii316@naver.com>
2023-07-30 23:40:30 +0900

    포르쉐홈페이지수정

--- porsche/index.js
+++ porsche/index.js
@@ -3,3 +3,21 @@ $("#toggle").click(function () {
   $("#toggle .bar").toggleClass("animate");
   $("#page").toggleClass("overlay");
 });
+
+// 헤더
+
+// 각 박스 안에 움직이는 이미지
+const movingImg = (evt) => {
+  let x = evt.clientX;
+  let y = evt.clientY;
+
+  $(".about").css({ right: -20 + x / 30, bottom: -10 + y / 30 });
+
+  requestAnimationFrame(movingImg);
+};
+$(".about").on("mousemove", movingImg);
+
+//페이드인
+AOS.init();
+
+//스크롤이미지
