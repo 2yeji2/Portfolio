153f032bb217659144c4d5ef6a574b83de0a086e
2yeji2 <ii316@naver.com>
2023-07-31 17:59:26 +0900

    포르쉐홈페이지퍼블리싱

--- porsche/index.js
+++ porsche/index.js
@@ -20,4 +20,26 @@ $(".about").on("mousemove", movingImg);
 //페이드인
 AOS.init();
 
+//opacity
+gsap.registerPlugin(ScrollTrigger);
+const fadeElement = document.querySelector(".scaleDown");
+gsap.to(fadeElement, {
+  opacity: 1, // 최종적으로 적용될 opacity 값
+  scrollTrigger: {
+    trigger: ".section2", // 스크롤 트리거로 사용할 요소
+    start: "top 100%", // 애니메이션이 시작할 위치 (top 80% 지점)
+    end: "bottom 10%", // 애니메이션이 끝날 위치 (bottom 20% 지점)
+    scrub: true, // 스크롤에 따라 부드럽게 애니메이션을 적용
+  },
+});
+
 //스크롤이미지
+gsap.set(".scaleDown", { xPercent: -50, yPercent: -50 });
+gsap.to(".scaleDown", {
+  scale: 107,
+  scrollTrigger: {
+    trigger: ".section2",
+    pin: ".section2",
+    scrub: true,
+  },
+});
