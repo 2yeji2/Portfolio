e66591dcb662f6bbb6dcfedb095705c4501bff98
2yeji2 <ii316@naver.com>
2023-08-12 18:55:46 +0900

    포르쉐 상세페이지 퍼블리싱

---
+++ Project/project4_porsche/project4.js
@@ -0,0 +1,47 @@
+//fade up
+AOS.init();
+
+// 다크모드
+$(".toggle_box span").on("click", () => {
+  $("header").children("a").toggleClass("dark_mode");
+  $("#project4").toggleClass("dark_mode");
+  $("#project4").children(".title").toggleClass("dark_mode");
+  $(".container").children(".porsche").children("h1").toggleClass("dark_mode");
+  $(".porsche").children("ul").children("li").toggleClass("dark_mode");
+  $("#overview").toggleClass("dark_mode");
+  $("#overview").children("h3").toggleClass("dark_mode");
+  $(".overview").children("ul").children("li").toggleClass("dark_mode");
+  $("#styleguide").toggleClass("dark_mode");
+  $("#styleguide").children("h3").toggleClass("dark_mode");
+  $("#styleguide").children("ul").children("li").toggleClass("dark_mode");
+  $("#styleguide").children(".color").toggleClass("dark_mode");
+  $("#Timeline").toggleClass("dark_mode");
+  $("#Timeline").children("h3").toggleClass("dark_mode");
+  $("#Timeline").children("ul").children("li").toggleClass("dark_mode");
+  $("#my-role").toggleClass("dark_mode");
+  $("#my-role").children("h3").toggleClass("dark_mode");
+  $("#my-role").children("ul").children("li").toggleClass("dark_mode");
+  $("#publishing").toggleClass("dark_mode");
+  $("#publishing").children("h3").toggleClass("dark_mode");
+  $("#publishing").children(".guide").toggleClass("dark_mode");
+  $(".publishing").children("ul").children("li").toggleClass("dark_mode");
+  $(".publishing")
+    .children("ul")
+    .children("li")
+    .children("p")
+    .toggleClass("dark_mode");
+  $(".publishing")
+    .children("ul")
+    .children("li")
+    .children(".tech")
+    .toggleClass("dark_mode");
+  $("#result").toggleClass("dark_mode");
+  $("#result").children("h3").toggleClass("dark_mode");
+  $("#result").children("ul").children("li").toggleClass("dark_mode");
+  $("#review").toggleClass("dark_mode");
+  $("#review").children("h3").toggleClass("dark_mode");
+  $("#review").children("ul").children("li").toggleClass("dark_mode");
+  $("footer").toggleClass("dark_mode");
+  $("footer").children("a").toggleClass("dark_mode");
+  $(".top-btn-svg").toggleClass("dark_mode");
+});
