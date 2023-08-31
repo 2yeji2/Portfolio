aa1bec0400a9ccff3bc042c0782d51bc1026d93f
2yeji2 <ii316@naver.com>
2023-08-31 21:25:49 +0900

    파일추가

---
+++ Project/project5_design/project5.js
@@ -0,0 +1,8 @@
+// 팝업
+function popup() {
+  var url = "popup.html";
+  var name = "popup test";
+  var option =
+    "width = 500, height = 500, top = 100, left = 200, location = no";
+  window.open(url, name, option);
+}
