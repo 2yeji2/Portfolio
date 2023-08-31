8ddaff23d59a317e3de193f96a02aac7c9c199c9
2yeji2 <ii316@naver.com>
2023-08-31 21:52:28 +0900

    사진추가

--- Project/project5_design/project5.js
+++ Project/project5_design/project5.js
@@ -1,8 +1,56 @@
-// 팝업
-function popup() {
-  var url = "popup.html";
-  var name = "popup test";
-  var option =
-    "width = 500, height = 500, top = 100, left = 200, location = no";
+//팝업;
+function popup1() {
+  var url =
+    "https://2yeji2.github.io/Portfolio/Project/project5_design/images/banner1.png";
+  var name = "popup1";
+  var option = "width=1500, height=700,top=100,left=200, location=no";
+  window.open(url, name, option);
+}
+
+function popup2() {
+  var url =
+    "https://2yeji2.github.io/Portfolio/Project/project5_design/images/banner2.png";
+  var name = "popup2";
+  var option = "width=1500,height=700,top=100,left=200,location=no";
+  window.open(url, name, option);
+}
+
+function popup3() {
+  var url =
+    "https://2yeji2.github.io/Portfolio/Project/project5_design/images/banner3.png";
+  var name = "popup2";
+  var option = "width=1500,height=700,top=100,left=200,location=no";
+  window.open(url, name, option);
+}
+
+function popup4() {
+  var url =
+    "https://2yeji2.github.io/Portfolio/Project/project5_design/images/banner4.png";
+  var name = "popup2";
+  var option = "width=1500,height=700,top=100,left=200,location=no";
+  window.open(url, name, option);
+}
+
+function popup5() {
+  var url =
+    "https://2yeji2.github.io/Portfolio/Project/project5_design/images/card.png";
+  var name = "popup2";
+  var option = "width=1500,height=900,top=100,left=200,location=no";
+  window.open(url, name, option);
+}
+
+function popup6() {
+  var url =
+    "https://2yeji2.github.io/Portfolio/Project/project5_design/images/menu.png";
+  var name = "popup2";
+  var option = "width=1500,height=900,top=100,left=200,location=no";
+  window.open(url, name, option);
+}
+
+function popup7() {
+  var url =
+    "https://2yeji2.github.io/Portfolio/Project/project5_design/images/menu.png";
+  var name = "popup2";
+  var option = "width=1500,height=900,top=100,left=200,location=no";
   window.open(url, name, option);
 }
